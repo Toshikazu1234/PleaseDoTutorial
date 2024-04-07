dfb2e079f872af3a1ec869ee58361e094791d633
RK <kanno.media808@gmail.com>
2024-04-07 16:21:05 -0700

    Add project to repo.

---
+++ PleaseDoTutorial/PleaseDoTutorial/PleaseDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialApp.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
