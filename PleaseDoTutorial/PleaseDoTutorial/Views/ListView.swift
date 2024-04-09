e2c7ec3f650c43393bb11422d7f0875a68ec9303
RK <kanno.media808@gmail.com>
2024-04-08 20:11:56 -0700

    Add ListView to project.

---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/8/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
