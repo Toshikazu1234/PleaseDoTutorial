ae80a8bd2468fa2693e11e2a7c14cce0968f49d9
RK <kanno.media808@gmail.com>
2024-04-13 09:44:45 -0700

    Add CTAButton to project.

---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/CTAButton.swift
@@ -0,0 +1,18 @@
+//
+//  CTAButton.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/13/24.
+//
+
+import SwiftUI
+
+struct CTAButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CTAButton()
+}
