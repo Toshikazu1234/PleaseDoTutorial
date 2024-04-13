ccb597b5a3ee5d0721af4c9fe4f0c46015db78c6
RK <kanno.media808@gmail.com>
2024-04-12 19:05:54 -0700

    Add PriorityMenu and StatusMenu to project.

---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/PriorityMenu.swift
@@ -0,0 +1,18 @@
+//
+//  PriorityMenu.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/12/24.
+//
+
+import SwiftUI
+
+struct PriorityMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PriorityMenu()
+}
---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/StatusMenu.swift
@@ -0,0 +1,18 @@
+//
+//  StatusMenu.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/12/24.
+//
+
+import SwiftUI
+
+struct StatusMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusMenu()
+}
