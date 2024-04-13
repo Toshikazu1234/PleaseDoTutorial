160500960059d1190235d514dd12ff7914edf45b
RK <kanno.media808@gmail.com>
2024-04-13 09:45:36 -0700

    Refactor reusable CTAButton to have injectable properties.

--- PleaseDoTutorial/PleaseDoTutorial/Views/CTAButton.swift
+++ PleaseDoTutorial/PleaseDoTutorial/Views/CTAButton.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct CTAButton: View {
+    let title: String
+    let action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.blue)
+                
+                Text(title)
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            }
+        }
+        .frame(maxHeight: 65)
     }
 }
 
 #Preview {
-    CTAButton()
+    CTAButton(title: "Confirm") {}
 }
