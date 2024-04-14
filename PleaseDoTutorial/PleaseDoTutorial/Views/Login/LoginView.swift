39a91272b186c6aa8a95a789d816aaaf19f100d7
RK <kanno.media808@gmail.com>
2024-04-13 18:39:07 -0700

    Add LoginTitleView.

---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/Login/LoginView.swift
@@ -0,0 +1,27 @@
+//
+//  LoginView.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/13/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Spacer()
+            
+            LoginTitleView()
+            
+            
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
