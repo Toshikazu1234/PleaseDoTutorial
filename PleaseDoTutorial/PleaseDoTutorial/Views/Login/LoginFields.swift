100a4bf20094bbb642bfadb3130c636f7af6316a
RK <kanno.media808@gmail.com>
2024-04-13 18:56:18 -0700

    Add EmailTextField to project.

--- PleaseDoTutorial/PleaseDoTutorial/Views/Login/LoginFields.swift
+++ PleaseDoTutorial/PleaseDoTutorial/Views/Login/LoginFields.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct LoginFields: View {
+    @State private var email = ""
+    
     var body: some View {
         VStack(spacing: 12) {
-            // TextField
+            EmailTextField(placeholder: "user@email.com", text: $email)
             
             // TextField
         }
+        .padding(.horizontal)
     }
 }
 
