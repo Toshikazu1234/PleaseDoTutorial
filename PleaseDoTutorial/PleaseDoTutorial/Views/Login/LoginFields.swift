51d3d63bc8d4287d8942f652b1aaa78ab20a73ab
RK <kanno.media808@gmail.com>
2024-04-13 18:46:21 -0700

    Add LoginFields to project.

---
+++ PleaseDoTutorial/PleaseDoTutorial/Views/Login/LoginFields.swift
@@ -0,0 +1,22 @@
+//
+//  LoginFields.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/13/24.
+//
+
+import SwiftUI
+
+struct LoginFields: View {
+    var body: some View {
+        VStack(spacing: 12) {
+            // TextField
+            
+            // TextField
+        }
+    }
+}
+
+#Preview {
+    LoginFields()
+}
--- PleaseDoTutorial/PleaseDoTutorial/Views/Login/LoginView.swift
+++ PleaseDoTutorial/PleaseDoTutorial/Views/Login/LoginView.swift
@@ -15,7 +15,10 @@ struct LoginView: View {
             
             LoginTitleView()
             
-            
+            Text("Login")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.horizontal)
             
             Spacer()
         }
