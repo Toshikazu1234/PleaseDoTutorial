d603c43fd388bc63bfcd71ccdacaccb5bd817ef9
RK <kanno.media808@gmail.com>
2024-04-18 12:56:58 -0700

    Add ItemsManager to project.

---
+++ PleaseDoTutorial/PleaseDoTutorial/App/ItemsManager.swift
@@ -0,0 +1,15 @@
+//
+//  ItemsManager.swift
+//  PleaseDoTutorial
+//
+//  Created by R K on 4/18/24.
+//
+
+import Foundation
+
+final class ItemsManager {
+    static let shared = ItemsManager()
+    private init() {}
+    
+    
+}
