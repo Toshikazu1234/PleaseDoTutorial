415d65558f9661ec3ff849f7aeea5f2c3b44aee9
RK <kanno.media808@gmail.com>
2024-04-18 18:01:04 -0700

    Fetch and sort batch items and pass to delegate.

--- PleaseDoTutorial/PleaseDoTutorial/App/ItemsManager.swift
+++ PleaseDoTutorial/PleaseDoTutorial/App/ItemsManager.swift
@@ -9,24 +9,68 @@ import Foundation
 import FirebaseAuth
 import FirebaseFirestore
 
+protocol ItemsManagerDelegate where Self: ListVM {
+    func didFetchBatchItems(_ items: [Status: [Item]])
+    func didAddItem(_ item: Item)
+    func didUpdateItem(_ item: Item)
+    func didDeleteItem(_ item: Item)
+}
+
 typealias IM = ItemsManager
 final class ItemsManager {
     static let shared = ItemsManager()
     private init() {}
     
+    weak var delegate: ItemsManagerDelegate?
     private let db = Firestore.firestore()
+    
+    private var isInitialFetch = true
+    
+    private var allItems: [Status: [Item]] = [
+        .todo: [],
+        .inProgress: [],
+        .done: []
+    ]
     
     func fetchItems() {
         guard let currentUser = Auth.auth().currentUser else { return }
         let id = currentUser.uid
-        
-        db.collection("Items").whereField("authorId", isEqualTo: id).addSnapshotListener { querySnapshot, error in
-            guard let documents = querySnapshot?.documents else {
-                print("Error fetching documents: \(error!)")
+        db.collection("Items").whereField("authorId", isEqualTo: id).addSnapshotListener { [weak self] snapshot, err in
+            if let err {
+                print("Error fetching documents: \(err)")
                 return
             }
-            let cities = documents.compactMap { $0["name"] }
-            print("Current cities in CA: \(cities)")
+            guard let self, let snapshot else { return }
+            snapshot.documentChanges.forEach { [weak self] diff in
+                guard let self else { return }
+                let data = diff.document.data()
+                let item = Item(data: data)
+                switch diff.type {
+                case .added:
+                    if isInitialFetch {
+                        allItems[item.status]?.append(item)
+                    } else {
+                        delegate?.didAddItem(item)
+                    }
+                case .modified:
+                    delegate?.didUpdateItem(item)
+                case .removed:
+                    delegate?.didDeleteItem(item)
+                }
+            }
+            guard isInitialFetch else { return }
+            sortBatchItems()
+            isInitialFetch = false
         }
+    }
+    
+    private func sortBatchItems() {
+        var sortedItems: [Status: [Item]] = [:]
+        allItems.keys.forEach { status in
+            sortedItems[status] = allItems[status]?.sorted(by: {
+                $0.startDate > $1.startDate
+            })
+        }
+        delegate?.didFetchBatchItems(sortedItems)
     }
 }
--- PleaseDoTutorial/PleaseDoTutorial/ViewModels/ListVM.swift
+++ PleaseDoTutorial/PleaseDoTutorial/ViewModels/ListVM.swift
@@ -10,22 +10,55 @@ import Foundation
 final class ListVM: ObservableObject {
     @Published var todoItems: [Item] = [
         Item(id: "abc123", authorId: "John Doe", title: "First item", description: "First description", startDate: .now, status: .todo, priority: .low),
-        Item(id: "123abc", authorId: "John Doe", title: "Second item", description: "Second description", startDate: .now + 5, status: .inProgress, priority: .medium),
+        Item(id: "123abc", authorId: "John Doe", title: "Second item", description: "Second description", startDate: .now + 5, status: .todo, priority: .medium),
         Item(id: "789xyz", authorId: "John Doe", title: "Third item", description: "Third description", startDate: .now + 10, status: .todo, priority: .low)
     ]
     @Published var inProgressItems: [Item] = [
-        Item(id: "abc123", authorId: "John Doe", title: "First item", description: "First description", startDate: .now, status: .todo, priority: .low),
+        Item(id: "abc123", authorId: "John Doe", title: "First item", description: "First description", startDate: .now, status: .inProgress, priority: .low),
         Item(id: "123abc", authorId: "John Doe", title: "Second item", description: "Second description", startDate: .now + 5, status: .inProgress, priority: .medium),
-        Item(id: "789xyz", authorId: "John Doe", title: "Third item", description: "Third description", startDate: .now + 10, status: .todo, priority: .low)
+        Item(id: "789xyz", authorId: "John Doe", title: "Third item", description: "Third description", startDate: .now + 10, status: .inProgress, priority: .low)
     ]
     @Published var doneItems: [Item] = [
-        Item(id: "abc123", authorId: "John Doe", title: "First item", description: "First description", startDate: .now, status: .todo, priority: .low),
-        Item(id: "123abc", authorId: "John Doe", title: "Second item", description: "Second description", startDate: .now + 5, status: .inProgress, priority: .medium),
-        Item(id: "789xyz", authorId: "John Doe", title: "Third item", description: "Third description", startDate: .now + 10, status: .todo, priority: .low)
+        Item(id: "abc123", authorId: "John Doe", title: "First item", description: "First description", startDate: .now, status: .done, priority: .low),
+        Item(id: "123abc", authorId: "John Doe", title: "Second item", description: "Second description", startDate: .now + 5, status: .done, priority: .medium),
+        Item(id: "789xyz", authorId: "John Doe", title: "Third item", description: "Third description", startDate: .now + 10, status: .done, priority: .low)
     ]
     @Published var unknown: [Item] = []
     
+    init() {
+        IM.shared.delegate = self
+    }
+    
     func fetchItems() {
         IM.shared.fetchItems()
+    }
+}
+
+extension ListVM: ItemsManagerDelegate {
+    func didFetchBatchItems(_ items: [Status: [Item]]) {
+        for status in items.keys {
+            switch status {
+            case .todo:
+                todoItems = items[status]!
+            case .inProgress:
+                inProgressItems = items[status]!
+            case .done:
+                doneItems = items[status]!
+            case .unknown:
+                continue
+            }
+        }
+    }
+    
+    func didAddItem(_ item: Item) {
+        <#code#>
+    }
+    
+    func didUpdateItem(_ item: Item) {
+        <#code#>
+    }
+    
+    func didDeleteItem(_ item: Item) {
+        <#code#>
     }
 }
