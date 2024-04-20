8dc20e8bdc6fff4bb0ef6a3b6642d47d2023304e
RK <kanno.media808@gmail.com>
2024-04-20 10:01:02 -0700

    Add function to convert Item to Firestore dictionary.

--- PleaseDoTutorial/PleaseDoTutorial/Models/Item.swift
+++ PleaseDoTutorial/PleaseDoTutorial/Models/Item.swift
@@ -50,6 +50,18 @@ struct Item: Identifiable, Equatable, Hashable {
     static func empty() -> Item {
         return Item(id: "", authorId: "", title: "", description: "", startDate: .now, status: .todo, priority: .low)
     }
+    
+    func toObject() -> [String: Any] {
+        var data: [String: Any] = [:]
+        data["id"] = id
+        data["authorId"] = authorId
+        data["title"] = title
+        data["description"] = description
+        data["startDate"] = Timestamp(date: startDate)
+        data["status"] = status.rawValue
+        data["priority"] = priority.rawValue
+        return data
+    }
 }
 
 enum Status: String, CaseIterable {
